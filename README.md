a993d1e9f07a6500a389b2f01dfdcc4113cc7cfc
Assper <theassper@gmail.com>
2020-01-03 18:00:41 +0200

    initial template

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-pug-template
+### Template For Simple Site
+
+#### Requirements
+* NodeJS: ^10.0.0
+* Git
+
+#### Installation
+* Clone or download repo
+* Run command inside repo folder: `npm i`
+
+#### Commands
+* `npm run build` - build production version
+* `npm run build:dev` - build development version
+* `npm run dev` - build and run dev server
+* `npm run js:analyze` - run js bundle analyzer
+* `npm run html:watch` - run watch for template changes
+* `npm run css:watch` - run watch for styles changes
+* `npm run js:watch` - run watch for scripts changes
+
+Template contain several command for separate build js, css or html and run dev server without build.
+
+#### Usage
+All project files contains in src folder and separate by scripts, styles and template folders.
+* Use [pug](https://pugjs.org/api/getting-started.html "pug") for build templates
+* Use [sass](https://sass-lang.com/documentation "sass") for create styles
+* And JS with [babel](https://babeljs.io/docs/en/ "babel") with ES6 modules and features
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+const isDev = process.env.NODE_ENV !== 'production'
+const out = path.join(__dirname, isDev ? 'dist' : 'build')
+
+const plugins = [
+  new CompressionPlugin({
+    filename: 'bundle.gz[query]',
+    algorithm: 'gzip',
+    test: /\.jsx?$/,
+    threshold: 10240,
+    minRatio: 0
+  }),
+  new CopyPlugin([{
+    from: 'src/assets/**/*',
+    to: `${out}/[1]/[name].[ext]`,
+    test: /.*\/(.+)\/.+\..+$/
+  }])
+]
+
+if (process.env.NODE_ENV === 'analyze') plugins.push(new BundleAnalyzerPlugin())
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: path.join(__dirname, 'src/scripts/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    path: out,
+    filename: 'js/bundle.js'
+  },
+  optimization: {
+    minimize: !isDev,
+    minimizer: [new TerserPlugin({ sourceMap: isDev })]
+  },
+  devtool: isDev && 'inline-source-map',
+  plugins,
+  devServer: {
+    contentBase: out,
+    compress: true,
+    hot: true,
+    port: 3000
+  }
+}
